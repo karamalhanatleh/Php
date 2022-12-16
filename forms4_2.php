48faff01765c0e81c1b365d67220187c5770383f
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2022-12-16 23:09:16 +0300

    Add files via upload

---
+++ forms4_2.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<form method="POST" action="forms4.php"> <br>
+<input type= "text" name="n1"><br>
+<input type= "text" name="n2"><br>
+<input type="submit" name="A" value="Add" />
+<input type="submit" name="S" value="Subtract" />
+<form>
+ 
