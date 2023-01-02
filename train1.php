cf022925975be0f6fabac9311d4dca467ca3796c
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-01-02 20:55:44 +0300

    Add files via upload

---
+++ train1.php
@@ -0,0 +1,18 @@
+<html>
+<body> 
+	<form method="post" action="">
+     id : <input type="id" name="id"  /><br>
+	 name : <input type="text" name="name"  /> <br>
+	 password :	<input type="password" name="password" /> <br>
+
+     <input type="submit" name="s" />
+     <input type="submit" name="show" />
+
+	</form>
+</body> </html>
+
+
+
+
+
+
